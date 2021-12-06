09fae0dd58055b6b7b21fb6bcf1211ac43914bd4
Mrsenior12 <61933232+Mrsenior12@users.noreply.github.com>
2021-12-06 19:16:43 +0100

    Surface calculation

--- src/main/java/pl/put/poznan/buildinginfo/rest/BuildingInfoController.java
+++ src/main/java/pl/put/poznan/buildinginfo/rest/BuildingInfoController.java
@@ -1,9 +1,12 @@
 package pl.put.poznan.buildinginfo.rest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
-import pl.put.poznan.buildinginfo.model.Building;
-import pl.put.poznan.buildinginfo.model.LocalizationType;
+import pl.put.poznan.buildinginfo.logic.SurfaceService;
+import pl.put.poznan.buildinginfo.model.*;
+
+import java.util.List;
 
 
 @RestController
@@ -11,18 +14,16 @@ import pl.put.poznan.buildinginfo.model.LocalizationType;
 public class BuildingInfoController {
 
     private static final Logger logger = LoggerFactory.getLogger(BuildingInfoController.class);
+    private final SurfaceService helper = new SurfaceService();
 
     @RequestMapping(method = RequestMethod.GET, produces = "application/json")
-    public String getSurface(@RequestBody Building building, @RequestParam(value="id") String id,
-                      @RequestParam(value= "type") LocalizationType localizationType) {
+    public SurfaceInformation getSurface(@RequestBody Building building, @RequestParam(value = "id") String id,
+                                         @RequestParam(value = "type") LocalizationType localizationType) {
 
-        // log the parameters
-     //   logger.debug(String.valueOf(building));
 
-        // perform the transformation, you should run your logic here, below is just a silly example
-        return "OK1";
+            return helper.calculateSurface(building,id,localizationType);
     }
-
 }
+
 
 
