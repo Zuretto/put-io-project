491c4341e688aee01088907984e471af07e30295
Zuretto <mikzur4@gmail.com>
2021-12-07 22:20:05 +0100

    add javadoc for the interface

--- src/main/java/pl/put/poznan/buildinginfo/logic/Localization.java
+++ src/main/java/pl/put/poznan/buildinginfo/logic/Localization.java
@@ -1,8 +1,21 @@
 package pl.put.poznan.buildinginfo.logic;
 
+/**
+ * Interface which is a Component in Composite project pattern
+ * provides method signatures for calculating properties of a localization
+ */
 public interface Localization {
 
+    /**
+     * method responsible for calculating surface of the localization
+     * @return Integer calculated surface
+     */
     Integer calculateSurface();
+
+    /**
+     * method responsible for calculating volume of the localization
+     * @return Integer calculated volume
+     */
     Integer calculateVolume();
 
 }
